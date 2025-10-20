4052e08e0331b0faeec7c17da523f27215eb4c89
Fernando Almaraz <fernandoalmarazdelaquintana@gmail.com>
2025-10-20 16:46:10 -0400

    chore: adding models

--- reborn_rentals_backend/database/migrations/2025_10_20_201052_create_orders_table.php
+++ reborn_rentals_backend/database/migrations/2025_10_20_201052_create_orders_table.php
@@ -13,6 +13,38 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('Id_jobs');
+            $table->foreign('Id_jobs')->references(
+                /* 'id' o 'Id_jobs' */ 'id'
+            )->on('jobs')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('Id_products');
+            $table->foreign('Id_products')->references(
+            )->on('products')->restrictOnDelete();
+
+            $table->unsignedBigInteger('Id_cupon')->nullable();
+            $table->foreign('Id_cupon')->references(
+                /* 'id' o 'Id_cupon' */ 'id'
+            )->on('cupons')->nullOnDelete();
+
+            $table->unsignedBigInteger('Id_users');
+            $table->foreign('Id_users')->references(
+                /* 'id' o 'Id_users' */ 'id'
+            )->on('users')->cascadeOnDelete();
+
+            // ----- Campos de la orden (según diagrama) -----
+            $table->decimal('total_amount', 10, 2);
+            $table->integer('quantity');
+            $table->boolean('status');
+
+            $table->decimal('discount_total', 10, 2)->nullable();
+            $table->dateTime('ordered_at');
+
+            $table->integer('payment_method'); // mapearás a tus códigos (p.ej. 1=efectivo, 2=tarjeta)
+            $table->decimal('tax_total', 10, 2)->nullable();
+
+            $table->string('transaction_id');
+            $table->text('notes');
             $table->timestamps();
         });
     }
