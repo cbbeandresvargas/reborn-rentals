1c4d0bef123c1aea3dd2206b4f4b18ce4a309a1b
Fernando Almaraz <fernandoalmarazdelaquintana@gmail.com>
2025-10-20 16:45:54 -0400

    chore: adding models

---
+++ reborn_rentals_backend/database/migrations/2025_10_20_201006_create_jobs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('latitude', 10, 7);
+            $table->longitude('latitude', 10, 7);
+            $table->date('date');
+            $table->time('time');
+            $table->text('notes')->nullable();
+            $table->bool('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
---
+++ reborn_rentals_backend/database/migrations/2025_10_20_201044_create_cupons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+             $table->string('discount_type'); // 'percent' o 'fixed'
+            $table->decimal('discount_value', 10, 2);
+            $table->unsignedSmallInteger('max_uses')->nullable();
+            $table->decimal('min_order_total', 10, 2)->nullable();
+            $table->dateTime('starts_at')->nullable();
+            $table->dateTime('expires_at')->nullable();
+             $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cupons');
+    }
+};
