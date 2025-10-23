071389841e8e2492e3ea9028fe832a65c8f032ab
Fabricio Crespo Rossel <crf0028382@est.univalle.edu>
2025-10-23 13:56:28 -0400

    Frontend Rentals

--- reborn-rental/src/data/products.js
+++
@@ -1,6 +0,0 @@
-export const PRODUCTS = [
-{ id: 'DP-120-200K', pricePerDay: 20, title: 'Large 70"x27" Concrete Washout Pan', subtitle: '(2.5 ton, 587 gallons capacity)', img: '/washout-large.png', code: 'DP-120-200K' },
-{ id: 'DP-120-300K', pricePerDay: 16, title: 'Small 70"x8" Concrete Washout Pan', subtitle: '(2.5 ton, 271 gallons capacity)', img: '/washout-small.png', code: 'DP-120-300K' },
-{ id: 'DP-7235-1200K', pricePerDay: 7, title: 'Small Concrete Washout Pan Lid (70"x8")', subtitle: '', img: '/washout-lid.png', code: 'DP-7235-1200K' },
-{ id: 'DP-7235-2200K', pricePerDay: 7, title: 'Large Concrete Washout Pan Lid (70"x27")', subtitle: '', img: '/washout-lid-large.png', code: 'DP-7235-2200K' },
-]
